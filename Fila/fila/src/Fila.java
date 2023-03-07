4683f8f14943fb382b49a663e19ecf129901a891
aclcampos <anaclaracamposbatista96@gmail.com>
2023-03-07 15:29:34 -0300

    Todos os estudos relacionados

---
+++ Fila/fila/src/Fila.java
@@ -0,0 +1,81 @@
+import java.sql.Ref;
+
+public class Fila<T> { 
+
+    private No<T> refNoEntrada;
+
+    public Fila() {
+        this.refNoEntrada = null;
+    } 
+   
+    public void enqueue(T object){
+        No novoNo = new No(object); 
+        novoNo.setRefNo(refNoEntrada);
+        refNoEntrada = novoNo; 
+    }
+   
+    public T first(){
+        if(!this.isEmpty()){
+            No primeiroNo = refNoEntrada; 
+            while(true){ 
+               if(primeiroNo.getRefNo() != null){
+                primeiroNo = primeiroNo.getRefNo();
+               }else{
+                break;
+               }
+            } 
+            return(T)primeiroNo.getObject(); 
+        }
+        return null;
+    }
+
+    public T dequeue(){
+        if(!this.isEmpty()){
+            No primeiroNo = refNoEntrada; 
+            No noAuxiliar = refNoEntrada;
+            while(true){
+               if(primeiroNo.getRefNo() != null){
+                noAuxiliar = primeiroNo;
+                primeiroNo = primeiroNo.getRefNo();
+               }else{
+                noAuxiliar.setRefNo(null);
+                break;
+               }
+            } 
+            return(T)primeiroNo.getObject();
+        }
+        return null;
+    }
+
+    public boolean isEmpty(){
+        return refNoEntrada == null?true : false; 
+    }
+
+    @Override
+    public String toString() {
+        String stringRetorno = ""; 
+        No noAuxiliar = refNoEntrada; 
+
+        if(refNoEntrada != null){
+            while (true){ 
+                stringRetorno += "[No{objeto= " + noAuxiliar.getObject() + "}]--->"; 
+                if(noAuxiliar.getRefNo() != null){
+                noAuxiliar = noAuxiliar.getRefNo(); 
+                }else{
+                    stringRetorno += null; 
+                    break;
+                }
+                
+            }
+
+        }else{
+            stringRetorno = "null"; 
+        }
+
+        return stringRetorno;
+    }
+
+    
+    
+}
+
